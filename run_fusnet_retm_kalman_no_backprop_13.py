6dd1fe1949320ad2f9e1794d144713c62f841843
RPX2001 <raveenpspathirana99@gmail.com>
2026-06-14 10:01:09 +0000

    update results with 0.5m/s

--- run_fusnet_retm_kalman_no_backprop_13.py
+++ run_fusnet_retm_kalman_no_backprop_13.py
@@ -157,13 +157,13 @@ def main():
 
     # ── Paths ──────────────────────────────────────────────────────────
     seq_dir = Path(
-        "/home/jaliya/eeg_speech/Julian/RetM_Workspace/Dataset/Moving_noise_sources/1"
+        "/home/jaliya/eeg_speech/Julian/RetM_Workspace/Dataset/Dataset_Folder/Moving_noise_sources/B/1"
     )
     checkpoint_path = Path(
         "/home/jaliya/eeg_speech/Julian/RetM_Workspace/ReTM_Research_Project/"
-        "best_checkpoint_A1_1_FUSENet_13_rctd.pth"
+        "best_checkpoint_A1_1_FUSENet_13_B.pth"
     )
-    out_dir = Path("results_fusnet_retm_kalman_1")
+    out_dir = Path("results_fusnet_retm_kalman_B_1")
     out_dir.mkdir(parents=True, exist_ok=True)
 
     fs_target     = 16000
